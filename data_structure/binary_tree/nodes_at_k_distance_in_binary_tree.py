b1ef8099155b831cb354805e43d3948c73a24423
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-24 21:01:05 +0100

    Keep decreasing distance until you hit 0

--- data_structure/binary_tree/nodes_at_k_distance_in_binary_tree.py
+++ data_structure/binary_tree/nodes_at_k_distance_in_binary_tree.py
@@ -3,26 +3,26 @@ from binary_search_tree_implementation import BinaryTree
 
 class MyBinaryTree(BinaryTree):
 
-    def nodes_at_distance(self, k):
-        if k < 0 or k > self.height():
+    def nodes_at_distance(self, distance):
+        if distance < 0 or distance > self.height():
             raise Exception('Illegal Argument')
 
         nodes = []
-        self.__nodes_at_distance(self.root, 0, k, nodes)
+        self.__nodes_at_distance(self.root, distance, nodes)
 
         return nodes
 
-    def __nodes_at_distance(self, node, distance, k, nodes):
+    def __nodes_at_distance(self, node, distance, nodes):
         if node is None:
             return
 
-        if distance == k:
+        if distance == 0:
             nodes.append(node.value)
             return
 
-        distance = distance + 1
-        self.__nodes_at_distance(node.left_child, distance, k, nodes)
-        self.__nodes_at_distance(node.right_child, distance, k, nodes)
+        distance = distance - 1
+        self.__nodes_at_distance(node.left_child, distance, nodes)
+        self.__nodes_at_distance(node.right_child, distance, nodes)
 
 
 if __name__ == "__main__":
