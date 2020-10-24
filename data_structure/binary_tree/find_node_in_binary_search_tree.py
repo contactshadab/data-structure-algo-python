1d87a5316b75e48620511f292da9f5098a17718f
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-24 20:01:33 +0100

    Find a node with given value in binary search tree

---
+++ data_structure/binary_tree/find_node_in_binary_search_tree.py
@@ -0,0 +1,41 @@
+from binary_search_tree_implementation import BinaryTree
+
+
+class MyBinarySeachTree(BinaryTree):
+
+    # Find in a Binary Search Tree
+    def find(self, value):
+        if self.root is None:
+            raise Exception('Empty tree')
+
+        current = self.root
+        while current is not None:
+            if value == current.value:
+                return True
+
+            if (value < current.value):
+                current = current.left_child
+            else:
+                current = current.right_child
+
+        return False
+
+    # Find in a Binary Search Tree with recursion
+    def find_with_recursion(self, value):
+        if self.root is None:
+            raise Exception('Empty tree')
+
+        return self.__find(self.root, value)
+
+
+if __name__ == "__main__":
+    # Populate binary search tree
+    binary_search_tree = MyBinarySeachTree()
+    binary_search_tree.insert(10)
+    binary_search_tree.insert(30)
+    binary_search_tree.insert(20)
+    binary_search_tree.insert(60)
+
+    # Find
+    print(30, binary_search_tree.find(30))
+    print(-100, binary_search_tree.find(-100))
