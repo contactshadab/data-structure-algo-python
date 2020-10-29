2e2203af5a6777d05ad854373a33973992b8afb5
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-29 21:14:05 +0000

    Implemented Insertion Sort

---
+++ algorithm/insertion_sort.py
@@ -0,0 +1,30 @@
+
+def insertion_sort(items):
+    if items is None:
+        raise Exception('Empty list')
+
+    for i in range(1, len(items)):
+        current = items[i]
+        j = i - 1
+        while j >= 0 and current < items[j]:
+
+            # Shift item to its right
+            items[j + 1] = items[j]
+
+            j = j - 1
+
+        items[j + 1] = current
+
+
+if __name__ == "__main__":
+    items1 = [30, 20, 40, 10, 50, 0]
+    insertion_sort(items1)
+    print(items1)
+
+    items2 = [30]
+    insertion_sort(items2)
+    print(items2)
+
+    items3 = []
+    insertion_sort(items3)
+    print(items3)
