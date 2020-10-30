429382159341e8a832c01ab638f77053c87868ae
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-30 22:24:10 +0000

    updated test

--- algorithm/searching/linear_search.py
+++ algorithm/searching/linear_search.py
@@ -8,7 +8,7 @@ def linear_search(items, target):
 
 if __name__ == "__main__":
     items = [7, 6, -3, 1, 5]
-    print(linear_search(items, 5))  # 0
+    print(linear_search(items, 5))  # 4
     print(linear_search(items, 7))  # 0
     print(linear_search(items, -3))  # 2
     print(linear_search(items, 100))    # -1
