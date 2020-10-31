cbdefe0cd777c8355af4eb5eaccf536dad43de58
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-31 15:39:03 +0000

    raise exception if array is null

--- data_structure/string/count_vowels.py
+++ data_structure/string/count_vowels.py
@@ -1,4 +1,7 @@
 def count(str):
+    if str is None:
+        raise Exception('Illegal argument')
+
     count = 0
     for ch in str:
         if ch in 'aeiou':
@@ -9,3 +12,4 @@ def count(str):
 
 print(count("I have a vowel"))
 print(count(""))
+print(count(None))
