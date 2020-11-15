aeb0bafb8d878bd5f2b5973d64795458f8a2b31a
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-11-15 12:16:25 +0000

    Task scheduler

---
+++ programs/leetcode/task_scheduler.py
@@ -0,0 +1,64 @@
+'''
+Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+Output: 8
+Explanation: 
+A -> B -> idle -> A -> B -> idle -> A -> B
+There is at least 2 units of time between any two same tasks.
+Example 2:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 0
+Output: 6
+Explanation: On this case any permutation of size 6 would work since n = 0.
+["A","A","A","B","B","B"]
+["A","B","A","B","A","B"]
+["B","B","B","A","A","A"]
+...
+And so on.
+Example 3:
+
+Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+Output: 16
+Explanation: 
+One possible solution is
+A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+ 
+
+Constraints:
+
+1 <= task.length <= 104
+tasks[i] is upper-case English letter.
+The integer n is in the range [0, 100].
+'''
+
+
+def least_interval(tasks, n):
+    frequencies = [0 for i in range(27)]
+    for task in tasks:
+        index = ord(task) - ord('A')
+        frequencies[index] += 1
+
+    frequencies.sort()
+
+    max_frequency = frequencies.pop()
+    max_idle = (max_frequency - 1) * n
+
+    while frequencies and max_idle > 0:
+        max_idle -= min(max_frequency - 1, frequencies.pop())
+
+    max_idle = max(max_idle, 0)
+
+    return len(tasks) + max_idle
+
+
+if __name__ == "__main__":
+    print(least_interval(["A", "A", "A", "B", "B", "B"], 2))
